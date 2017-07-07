4f81ce02662f164d9bad6b8b3e28e5ac56772a22
Kino <jose33201@hotmail.com>
2017-07-06 21:08:29 -0400

    #141 - Poison now afflicts damage to all mobs

--- src/main/java/com/legacy/aether/common/entities/projectile/darts/EntityDartPoison.java
+++ src/main/java/com/legacy/aether/common/entities/projectile/darts/EntityDartPoison.java
@@ -1,10 +1,18 @@
 package com.legacy.aether.common.entities.projectile.darts;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 
+import com.legacy.aether.common.entities.movement.AetherPoisonMovement;
 import com.legacy.aether.common.items.ItemsAether;
 import com.legacy.aether.common.networking.AetherNetworkingManager;
 import com.legacy.aether.common.networking.packets.PacketSendPoison;
@@ -12,6 +20,10 @@ import com.legacy.aether.common.player.PlayerAether;
 
 public class EntityDartPoison extends EntityDartBase
 {
+
+	public EntityLivingBase victim;
+
+	public AetherPoisonMovement poison;
 
     public EntityDartPoison(World worldIn)
     {
@@ -29,27 +41,74 @@ public class EntityDartPoison extends EntityDartBase
         this.setDamage(0);
     }
 
+    public void onUpdate()
+    {
+        super.onUpdate();
+
+        if (this.victim != null)
+        {
+        	if (this.victim.isDead || this.poison.poisonTime == 0)
+        	{
+        		this.setDead();
+
+        		return;
+        	}
+
+        	if (this.shootingEntity != null)
+        	{
+                if (this.shootingEntity.worldObj instanceof WorldServer)
+                {
+                	((WorldServer)this.shootingEntity.worldObj).spawnParticle(EnumParticleTypes.ITEM_CRACK, this.victim.posX, this.victim.getEntityBoundingBox().minY + this.victim.height * 0.8D, this.victim.posZ, 2, 0.0D, 0.0D, 0.0D, 0.0625D, new int[] {Item.getIdFromItem(Items.DYE), 1});
+                }
+        	}
+
+        	this.isDead = false;
+        	this.poison.onUpdate();
+        	this.setInvisible(true);
+        	this.setPosition(this.victim.posX, this.victim.posY, this.victim.posZ);
+        }
+    }
+
+    @Override
+    public void onCollideWithPlayer(EntityPlayer entityIn)
+    {
+    	if (this.victim == null)
+    	{
+    		super.onCollideWithPlayer(entityIn);
+    	}
+    }
+
     @Override
     protected void arrowHit(EntityLivingBase living)
     {
     	super.arrowHit(living);
 
+    	this.victim = living;
+    	this.poison = new AetherPoisonMovement(this.victim);
+    	this.poison.afflictPoison();
+
     	if (living instanceof EntityPlayer)
     	{
             EntityPlayer ent = (EntityPlayer)living;
-            PlayerAether player = PlayerAether.get(ent);
 
-            if (!player.isPoisoned())
+            if (!this.worldObj.isRemote)
             {
-                if (!this.worldObj.isRemote)
-                {
-                	player.afflictPoison();
-                	AetherNetworkingManager.sendToAll(new PacketSendPoison(ent));
-
-                    this.setDead();
-                }
+            	AetherNetworkingManager.sendToAll(new PacketSendPoison(ent));
             }
     	}
+
+    	this.isDead = false;
+    }
+
+    @Override
+    protected Entity findEntityOnPath(Vec3d start, Vec3d end)
+    {
+    	if (this.victim != null)
+    	{
+    		return null;
+    	}
+
+    	return super.findEntityOnPath(start, end);
     }
 
 	@Override
