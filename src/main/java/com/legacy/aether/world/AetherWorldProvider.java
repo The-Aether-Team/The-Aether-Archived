fe72de0a2b8b19a2e09b0702f09715acc81ca983
Kino <jose33201@hotmail.com>
2017-12-09 16:58:55 -0500

    empty weather renderer added

--- src/main/java/com/legacy/aether/world/AetherWorldProvider.java
+++ src/main/java/com/legacy/aether/world/AetherWorldProvider.java
@@ -1,11 +1,16 @@
 package com.legacy.aether.world;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraftforge.client.IRenderHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class AetherWorldProvider extends WorldProvider
 {
@@ -152,5 +157,18 @@ public class AetherWorldProvider extends WorldProvider
 	{
 		return AetherWorld.aether_dimension_type;
 	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+    public net.minecraftforge.client.IRenderHandler getWeatherRenderer()
+    {
+    	return new IRenderHandler()
+    	{
+			@Override
+			public void render(float partialTicks, net.minecraft.client.multiplayer.WorldClient world, net.minecraft.client.Minecraft mc)
+			{
+
+			}};
+    }
 
 }
