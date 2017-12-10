c4434e7b141939cd2ab358b7a056204fa560c87b
Kino <jose33201@hotmail.com>
2017-12-09 20:37:43 -0500

    Zanite and gravitite blocks power beacons

--- src/main/java/com/legacy/aether/blocks/natural/enchanted/BlockEnchantedGravitite.java
+++ src/main/java/com/legacy/aether/blocks/natural/enchanted/BlockEnchantedGravitite.java
@@ -2,6 +2,8 @@ package com.legacy.aether.blocks.natural.enchanted;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 import com.legacy.aether.blocks.util.BlockFloating;
 
@@ -15,5 +17,11 @@ public class BlockEnchantedGravitite extends BlockFloating
 		this.setHardness(5F);
 		this.setSoundType(SoundType.METAL);
 	}
+
+	@Override
+    public boolean isBeaconBase(IBlockAccess worldObj, BlockPos pos, BlockPos beacon)
+    {
+    	return true;
+    }
 
 }
