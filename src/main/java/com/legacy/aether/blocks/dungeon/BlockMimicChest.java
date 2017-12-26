c9aba997e63fa821cb97abdb94dd1e7c532453a7
KingPhygieBoo <wheresperry41@gmail.com>
2017-12-25 22:55:15 -0600

    Mimic Chests make a sound when opened now.

--- src/main/java/com/legacy/aether/blocks/dungeon/BlockMimicChest.java
+++ src/main/java/com/legacy/aether/blocks/dungeon/BlockMimicChest.java
@@ -4,10 +4,12 @@ import net.minecraft.block.BlockChest;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
@@ -35,6 +37,16 @@ public class BlockMimicChest extends BlockChest
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ)
 	{
 		this.spawnMimic(worldIn, playerIn, pos);
+		
+		int i = pos.getX();
+        int j = pos.getY();
+        int k = pos.getZ();
+        
+        double d1 = (double)i + 0.5D;
+        double d2 = (double)k + 0.5D;
+		
+		 worldIn.playSound((EntityPlayer)null, d1, (double)j + 0.5D, d2, SoundEvents.BLOCK_CHEST_OPEN, SoundCategory.BLOCKS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
+
 
 		return true;
 	}
