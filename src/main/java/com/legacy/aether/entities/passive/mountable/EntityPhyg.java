f90e835f98dc1525088294c3465d8e45c1f00e71
KingPhygieBoo <wheresperry41@gmail.com>
2017-12-26 17:09:45 -0600

    Unused imports.

--- src/main/java/com/legacy/aether/entities/passive/mountable/EntityPhyg.java
+++ src/main/java/com/legacy/aether/entities/passive/mountable/EntityPhyg.java
@@ -14,7 +14,6 @@ import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
