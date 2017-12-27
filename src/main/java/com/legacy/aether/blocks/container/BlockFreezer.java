5514e43cb0713742a7d15a31fc0811bf6ac141e8
KingPhygieBoo <wheresperry41@gmail.com>
2017-12-26 18:28:48 -0600

    Tile entity particles and sounds.

--- src/main/java/com/legacy/aether/blocks/container/BlockFreezer.java
+++ src/main/java/com/legacy/aether/blocks/container/BlockFreezer.java
@@ -5,14 +5,19 @@ import java.util.Random;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import com.legacy.aether.Aether;
 import com.legacy.aether.blocks.BlocksAether;
@@ -47,6 +52,29 @@ public class BlockFreezer extends BlockAetherContainer
     	player.openGui(Aether.instance, AetherGuiHandler.freezer, world, pos.getX(), pos.getY(), pos.getZ());
 
         return true;
+    }
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random random)
+    {
+		TileEntityFreezer tileentity = (TileEntityFreezer)world.getTileEntity(pos);
+
+		if(tileentity.isBurning())
+		{
+			float f = (float)pos.getX() + 0.5F;
+			float f1 = (float)pos.getY() + 1.0F + (random.nextFloat() * 6F) / 16F;
+			float f2 = (float)pos.getZ() + 0.5F;
+
+			world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, f, f1, f2, 0.0D, 0.0D, 0.0D);
+			
+			for (int i = 0; i < 10; ++i)
+        	{
+				world.spawnParticle(EnumParticleTypes.SNOW_SHOVEL, f, f1, f2, 0.0D, 0.0D, 0.0D);
+        	}
+			
+                world.playSound((double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 3.0F, false);
+		}
     }
 
 	@Override
