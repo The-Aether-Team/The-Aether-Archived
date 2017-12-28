f9b627228dfc24491c8fd8bd17dc34346d6e7423
KingPhygieBoo <wheresperry41@gmail.com>
2017-12-27 23:14:53 -0600

    Yet another cool little thing.

--- src/main/java/com/legacy/aether/client/renders/entities/FireMinionRenderer.java
+++ src/main/java/com/legacy/aether/client/renders/entities/FireMinionRenderer.java
@@ -11,6 +11,8 @@ public class FireMinionRenderer extends RenderBiped<EntityFireMinion>
 {
 
     private static final ResourceLocation SPIRIT = new ResourceLocation("aether_legacy", "textures/bosses/sun_spirit/sun_spirit.png");
+    
+    private static final ResourceLocation FROZEN_SPIRIT = new ResourceLocation("aether_legacy", "textures/bosses/sun_spirit/frozen_sun_spirit.png");
 
     public FireMinionRenderer(RenderManager renderManager)
     {
@@ -22,7 +24,14 @@ public class FireMinionRenderer extends RenderBiped<EntityFireMinion>
 	@Override
     protected ResourceLocation getEntityTexture(EntityFireMinion entity)
     {
-		return SPIRIT;
+		if (entity.hasCustomName() && "JorgeQ".equals(entity.getCustomNameTag()))
+		{
+			return FROZEN_SPIRIT;
+		}
+		else
+		{
+			return SPIRIT;
+		}
     }
 
 }
