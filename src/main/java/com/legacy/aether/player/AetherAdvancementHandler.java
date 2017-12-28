3eb6a7cc03aa128e7e958358709fbbadfc72393a
Kino <jose33201@hotmail.com>
2017-12-28 16:34:10 -0500

    Fixed aether advancements not playing jingle

--- src/main/java/com/legacy/aether/player/AetherAdvancementHandler.java
+++ src/main/java/com/legacy/aether/player/AetherAdvancementHandler.java
@@ -1,9 +1,5 @@
 package com.legacy.aether.player;
 
-import com.legacy.aether.items.ItemsAether;
-import com.legacy.aether.networking.AetherNetworkingManager;
-import com.legacy.aether.networking.packets.PacketAchievement;
-
 import net.minecraft.advancements.Advancement;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,39 +8,54 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import com.legacy.aether.items.ItemsAether;
+import com.legacy.aether.networking.AetherNetworkingManager;
+import com.legacy.aether.networking.packets.PacketAchievement;
+
 public class AetherAdvancementHandler
 {
+
 	@SubscribeEvent
 	public void onAchievementGet(AdvancementEvent event)
 	{
 		Advancement advancement = event.getAdvancement();
-		EntityPlayer player = event.getEntityPlayer();
 
-		String unlocalizedName = advancement.getDisplayText().getUnformattedText();
-
-		if (!unlocalizedName.contains("aether_legacy"))
+		if (advancement.getDisplay() != null && advancement.getDisplay().getIcon().getItem() != null)
 		{
-			return;
-		}
+			ItemStack stack = advancement.getDisplay().getIcon();
+			EntityPlayer player = event.getEntityPlayer();
 
-		int achievementType = unlocalizedName.contains("bronze_dungeon") ? 1 : unlocalizedName.contains("silver_dungeon") ? 2 : 0;
+			int achievementType = 0;
 
-		if (!player.world.isRemote)
-		{
-			if (unlocalizedName.contains("enter_aether"))
+			if (stack.getItem() == ItemsAether.dungeon_key)
 			{
-				if (!player.inventory.addItemStackToInventory(new ItemStack(ItemsAether.lore_book)))
-				{
-					player.world.spawnEntity(new EntityItem(player.world, player.posX, player.posY, player.posZ, new ItemStack(ItemsAether.lore_book)));
-				}
-
-				if (!player.inventory.addItemStackToInventory(new ItemStack(ItemsAether.golden_parachute)))
-				{
-					player.world.spawnEntity(new EntityItem(player.world, player.posX, player.posY, player.posZ, new ItemStack(ItemsAether.golden_parachute)));
-				}
+				achievementType = (stack.getMetadata() == 0 ? 1 : stack.getMetadata() == 1 ? 2 : 0);
 			}
 
-			AetherNetworkingManager.sendTo(new PacketAchievement(achievementType), (EntityPlayerMP) player);
+			if (!player.world.isRemote)
+			{
+				boolean sendJingle = stack.getItem().getRegistryName().getResourceDomain().equals("aether_legacy");
+
+				if (advancement.getDisplay().getDescription().getFormattedText().contains("Hostile Paradise"))
+				{
+					if (!player.inventory.addItemStackToInventory(new ItemStack(ItemsAether.lore_book)))
+					{
+						player.world.spawnEntity(new EntityItem(player.world, player.posX, player.posY, player.posZ, new ItemStack(ItemsAether.lore_book)));
+					}
+
+					if (!player.inventory.addItemStackToInventory(new ItemStack(ItemsAether.golden_parachute)))
+					{
+						player.world.spawnEntity(new EntityItem(player.world, player.posX, player.posY, player.posZ, new ItemStack(ItemsAether.golden_parachute)));
+					}
+
+					sendJingle = true;
+				}
+
+				if (sendJingle)
+				{
+					AetherNetworkingManager.sendTo(new PacketAchievement(achievementType), (EntityPlayerMP) player);
+				}
+			}
 		}
 	}
 }
