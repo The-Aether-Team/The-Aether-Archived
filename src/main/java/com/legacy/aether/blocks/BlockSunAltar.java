4edae96c001aaeca8b68e401b6d05c3041b89ac1
Kino <jose33201@hotmail.com>
2017-12-27 22:07:59 -0500

    Fixed Altar crash on servers

--- src/main/java/com/legacy/aether/blocks/BlockSunAltar.java
+++ src/main/java/com/legacy/aether/blocks/BlockSunAltar.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumFacing;
@@ -30,7 +31,11 @@ public class BlockSunAltar extends Block
     {
 		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
 
-		if ((server.isDedicatedServer() && server.getPlayerList().canSendCommands(player.getGameProfile()) && player.capabilities.isCreativeMode) || !server.isDedicatedServer())
+		if (server != null && ((server.isDedicatedServer() && server.getPlayerList().canSendCommands(player.getGameProfile()) && player.capabilities.isCreativeMode) || !server.isDedicatedServer()))
+		{
+			Aether.proxy.openSunAltar();
+		}
+		else if (player instanceof EntityPlayerSP && ((EntityPlayerSP)player).getPermissionLevel() > 0 && player.capabilities.isCreativeMode)
 		{
 			Aether.proxy.openSunAltar();
 		}
