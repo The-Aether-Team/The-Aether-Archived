a6d4f1bbb80fc509a21cab6aace8f1757528622f
KingPhygieBoo <wheresperry41@gmail.com>
2018-01-13 02:32:13 -0600

    The Valkyrie Queen can get through player made barricades now.

--- src/main/java/com/legacy/aether/entities/bosses/valkyrie_queen/EntityValkyrieQueen.java
+++ src/main/java/com/legacy/aether/entities/bosses/valkyrie_queen/EntityValkyrieQueen.java
@@ -1,9 +1,11 @@
 package com.legacy.aether.entities.bosses.valkyrie_queen;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -35,6 +37,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import com.legacy.aether.Aether;
 import com.legacy.aether.blocks.BlocksAether;
 import com.legacy.aether.blocks.dungeon.BlockDungeonBase;
+import com.legacy.aether.blocks.natural.BlockAetherLeaves;
+import com.legacy.aether.blocks.natural.BlockAetherLog;
+import com.legacy.aether.blocks.util.EnumLeafType;
+import com.legacy.aether.blocks.util.EnumLogType;
 import com.legacy.aether.blocks.util.EnumStoneType;
 import com.legacy.aether.entities.ai.EntityAIAttackContinuously;
 import com.legacy.aether.entities.ai.valkyrie_queen.ValkyrieQueenAIWander;
@@ -312,7 +318,7 @@ public class EntityValkyrieQueen extends EntityMob
     {
     	this.lastMotionY = motionY;
         super.onUpdate();
-
+        
         if (!this.onGround && this.getAttackTarget() != null && this.lastMotionY >= 0.0D && motionY < 0.0D && getDistance(this.getAttackTarget()) <= 16F && canEntityBeSeen(this.getAttackTarget())) 
         {
             double a = this.getAttackTarget().posX - posX;
@@ -376,6 +382,30 @@ public class EntityValkyrieQueen extends EntityMob
                 this.setDead();
             }
         }
+        
+        int i2 = (MathHelper.floor(this.posX) - 1) + this.rand.nextInt(3);
+        int j2 = MathHelper.floor(this.posY) + this.rand.nextInt(2);
+        int k2 = (MathHelper.floor(this.posZ) - 1) + this.rand.nextInt(3);
+        
+        
+        if(this.onGround && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == BlocksAether.locked_dungeon_block) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == BlocksAether.treasure_chest) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == Blocks.AIR) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == Blocks.WOOL) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == BlocksAether.ambrosium_torch) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == Blocks.WATER) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == BlocksAether.aether_log.getDefaultState().withProperty(BlockAetherLog.wood_type, EnumLogType.Oak).withProperty(BlockAetherLog.double_drop, Boolean.FALSE)) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == (BlocksAether.aether_leaves) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == Blocks.FIRE) && !(this.world.getBlockState(new BlockPos.MutableBlockPos().setPos(i2, j2, k2)).getBlock() == BlocksAether.golden_oak_sapling)))
+        {
+        	//System.out.println("m");
+        	this.world.setBlockState(new BlockPos(i2, j2, k2), Blocks.AIR.getDefaultState());
+        }
+    }
+    
+    @Override
+    public void move(MoverType type, double x, double y, double z)
+    {
+		if (this.isBossReady())
+		{
+			super.move(type, x, y, z);
+		}
+		else
+		{
+			super.move(type, 0, y, 0);
+		}
     }
 
     @Override
