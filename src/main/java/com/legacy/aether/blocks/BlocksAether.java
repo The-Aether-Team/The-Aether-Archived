2513507818fb1f94b96c1f2190f8e70c761229d2
KingPhygieBoo <wheresperry41@gmail.com>
2018-01-14 22:04:25 -0600

    Added the Skyroot Bookshelf!

--- src/main/java/com/legacy/aether/blocks/BlocksAether.java
+++ src/main/java/com/legacy/aether/blocks/BlocksAether.java
@@ -24,6 +24,7 @@ import com.legacy.aether.blocks.decorative.BlockAetherWall;
 import com.legacy.aether.blocks.decorative.BlockAmbrosiumTorch;
 import com.legacy.aether.blocks.decorative.BlockPresent;
 import com.legacy.aether.blocks.decorative.BlockQuicksoilGlass;
+import com.legacy.aether.blocks.decorative.BlockSkyrootBookshelf;
 import com.legacy.aether.blocks.decorative.BlockSkyrootPlank;
 import com.legacy.aether.blocks.decorative.BlockZanite;
 import com.legacy.aether.blocks.dungeon.BlockDungeonBase;
@@ -106,6 +107,8 @@ public class BlocksAether
 	public static Block holiday_leaves, present;
 
 	public static Block sun_altar;
+	
+	public static Block skyroot_bookshelf;
 
 	public static void initialization()
 	{
@@ -190,6 +193,8 @@ public class BlocksAether
 		aerogel_slab = registerSlab("aerogel_slab", new BlockAerogelSlab("aerogel_slab", false, Material.IRON).setHardness(0.5F).setResistance(999F), aerogel_double_slab);
 
 		sun_altar = register("sun_altar", new BlockSunAltar());
+		
+		skyroot_bookshelf = register("skyroot_bookshelf", new BlockSkyrootBookshelf());
 	}
 
 	public static Block registerSlab(String name, Block slab1, Block slab2)
