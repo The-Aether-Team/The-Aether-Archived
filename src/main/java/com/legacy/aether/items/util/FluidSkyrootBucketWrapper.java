7945adbe66b0d237bf6536db9e2d60bf241c3b08
Kino <jose33201@hotmail.com>
2019-01-31 00:26:32 -0500

    #351 - Fixed skyroot bucket wrapper

--- src/main/java/com/legacy/aether/items/util/FluidSkyrootBucketWrapper.java
+++ src/main/java/com/legacy/aether/items/util/FluidSkyrootBucketWrapper.java
@@ -1,60 +1,176 @@
 package com.legacy.aether.items.util;
 
-import javax.annotation.Nullable;
-
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.FluidTankProperties;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
 import com.legacy.aether.items.ItemsAether;
 
-public class FluidSkyrootBucketWrapper extends FluidBucketWrapper
+public class FluidSkyrootBucketWrapper implements IFluidHandlerItem, ICapabilityProvider
 {
+
+	private ItemStack container;
 
 	public FluidSkyrootBucketWrapper(ItemStack container)
 	{
-		super(container);
+		this.container = container;
 	}
 
-    public boolean canFillFluidType(FluidStack fluid)
-    {
-        if (fluid.getFluid() == FluidRegistry.WATER)
-        {
-            return true;
-        }
- 
-        return false;
-    }
+	@Override
+	public ItemStack getContainer()
+	{
+		return container;
+	}
 
-    @Nullable
-    public FluidStack getFluid()
-    {
-        Item item = container.getItem();
-        int meta = container.getItemDamage();
+	public boolean canFillFluidType(FluidStack fluidStack)
+	{
+		String fluidName = fluidStack.getFluid().getName();
 
-        if (item == ItemsAether.skyroot_bucket && meta == 1)
-        {
-            return new FluidStack(FluidRegistry.WATER, Fluid.BUCKET_VOLUME);
-        }
-        else
-        {
-            return null;
-        }
-    }
+		return fluidStack.getFluid() == FluidRegistry.WATER || fluidName.equals("inebriation") || fluidName.equals("remedy") || fluidName.equals("milk");
+	}
 
-    protected void setFluid(Fluid fluid) 
-    {
-        if (fluid == null)
-        {
-            container.deserializeNBT(new ItemStack(ItemsAether.skyroot_bucket).serializeNBT());
-        }
-        else if (fluid == FluidRegistry.WATER)
-        {
-            container.deserializeNBT(new ItemStack(ItemsAether.skyroot_bucket, 1, 1).serializeNBT());
-        }
-    }
+	public FluidStack getFluid()
+	{
+		if (this.container.getItem() == ItemsAether.skyroot_bucket && this.container.getItemDamage() == EnumSkyrootBucketType.Water.getMeta())
+		{
+			return new FluidStack(FluidRegistry.WATER, Fluid.BUCKET_VOLUME);
+		}
+		else if (this.container.getItem() == ItemsAether.skyroot_bucket && this.container.getItemDamage() == EnumSkyrootBucketType.Poison.getMeta())
+		{
+			return FluidRegistry.getFluidStack("inebriation", Fluid.BUCKET_VOLUME);
+		}
+		else if (this.container.getItem() == ItemsAether.skyroot_bucket && this.container.getItemDamage() == EnumSkyrootBucketType.Remedy.getMeta())
+		{
+			return FluidRegistry.getFluidStack("remedy", Fluid.BUCKET_VOLUME);
+		}
+		else if (this.container.getItem() == ItemsAether.skyroot_bucket && this.container.getItemDamage() == EnumSkyrootBucketType.Milk.getMeta())
+		{
+			return FluidRegistry.getFluidStack("milk", Fluid.BUCKET_VOLUME);
+		}
+
+		return null;
+	}
+
+	protected void setFluid(FluidStack fluidStack)
+	{
+		if (fluidStack == null)
+		{
+			this.container = new ItemStack(ItemsAether.skyroot_bucket);
+
+			return;
+		}
+
+		String fluidName = fluidStack.getFluid().getName();
+
+		if (fluidStack.getFluid() == FluidRegistry.WATER)
+		{
+			this.container = new ItemStack(ItemsAether.skyroot_bucket, 1, EnumSkyrootBucketType.Water.getMeta());
+		}
+		else if (fluidName.equals("inebriation"))
+		{
+			this.container = new ItemStack(ItemsAether.skyroot_bucket, 1, EnumSkyrootBucketType.Poison.getMeta());
+		}
+		else if (fluidName.equals("remedy"))
+		{
+			this.container = new ItemStack(ItemsAether.skyroot_bucket, 1, EnumSkyrootBucketType.Remedy.getMeta());
+		}
+		else if (fluidName.equals("milk"))
+		{
+			this.container = new ItemStack(ItemsAether.skyroot_bucket, 1, EnumSkyrootBucketType.Milk.getMeta());
+		}
+	}
+
+	@Override
+	public IFluidTankProperties[] getTankProperties()
+	{
+		return new FluidTankProperties[] { new FluidTankProperties(this.getFluid(), Fluid.BUCKET_VOLUME) };
+	}
+
+	@Override
+	public int fill(FluidStack resource, boolean doFill)
+	{
+		if (this.container.getCount() != 1 || resource == null || resource.amount < Fluid.BUCKET_VOLUME || this.getFluid() != null || !this.canFillFluidType(resource))
+		{
+			return 0;
+		}
+
+		if (doFill)
+		{
+			this.setFluid(resource);
+		}
+
+		return Fluid.BUCKET_VOLUME;
+	}
+
+	@Override
+	public FluidStack drain(FluidStack resource, boolean doDrain)
+	{
+		if (this.container.getCount() != 1 || resource == null || resource.amount < Fluid.BUCKET_VOLUME)
+		{
+			return null;
+		}
+
+		FluidStack fluidStack = this.getFluid();
+
+		if (fluidStack != null && fluidStack.isFluidEqual(resource))
+		{
+			if (doDrain)
+			{
+				this.setFluid(null);
+			}
+
+			return fluidStack;
+		}
+
+		return null;
+	}
+
+	@Override
+	public FluidStack drain(int maxDrain, boolean doDrain)
+	{
+		if (this.container.getCount() != 1 || maxDrain < Fluid.BUCKET_VOLUME)
+		{
+			return null;
+		}
+
+		FluidStack fluidStack = this.getFluid();
+
+		if (fluidStack != null)
+		{
+			if (doDrain)
+			{
+				this.setFluid(null);
+			}
+
+			return fluidStack;
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+	{
+		return capability == CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY;
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+	{
+		if (capability == CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY)
+		{
+			return CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY.cast(this);
+		}
+
+		return null;
+	}
 
 }
