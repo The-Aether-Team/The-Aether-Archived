ab2815f83644807dd4d09292d530880f218ed28f
KingPhygieBoo <wheresperry41@gmail.com>
2019-01-31 01:13:52 -0600

    Fixed the weather render being dumb dumb

--- src/main/java/com/legacy/aether/world/AetherWorldProvider.java
+++ src/main/java/com/legacy/aether/world/AetherWorldProvider.java
@@ -6,7 +6,6 @@ import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraftforge.client.IRenderHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -155,18 +154,5 @@ public class AetherWorldProvider extends WorldProvider
 	{
 		return AetherWorld.aether_dimension_type;
 	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-    public net.minecraftforge.client.IRenderHandler getWeatherRenderer()
-    {
-    	return new IRenderHandler()
-    	{
-			@Override
-			public void render(float partialTicks, net.minecraft.client.multiplayer.WorldClient world, net.minecraft.client.Minecraft mc)
-			{
-
-			}};
-    }
 
 }
