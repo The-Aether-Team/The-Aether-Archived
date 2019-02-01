f70cbd07fd6ba252a58e6b5668d7a8a6ecead150
Kino <jose33201@hotmail.com>
2019-01-31 19:59:43 -0500

    removed import

--- src/main/java/com/legacy/aether/player/PlayerAether.java
+++ src/main/java/com/legacy/aether/player/PlayerAether.java
@@ -12,7 +12,6 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
