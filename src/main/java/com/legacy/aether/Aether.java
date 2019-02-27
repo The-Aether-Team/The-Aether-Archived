718491ae660609a7ca63cc29e3bd0f37d0828081
Kino <jose33201@hotmail.com>
2019-02-27 14:20:02 -0500

    updated version number

--- src/main/java/com/legacy/aether/Aether.java
+++ src/main/java/com/legacy/aether/Aether.java
@@ -34,7 +34,7 @@ public class Aether
 
 	public static final String modid = "aether_legacy";
 
-	public static final String version = "1.12.2-v1.4.0";
+	public static final String version = "1.12.2-v1.4.1";
 
 	@Instance(Aether.modid)
 	public static Aether instance;
