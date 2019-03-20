394b72983ad5fe048c383b20688957f6a64852c8
KingPhygieBoo <wheresperry41@gmail.com>
2019-03-19 21:53:37 -0500

    Fixed dangerous alternative prefixes

--- src/main/java/com/legacy/aether/tile_entities/AetherTileEntities.java
+++ src/main/java/com/legacy/aether/tile_entities/AetherTileEntities.java
@@ -1,5 +1,7 @@
 package com.legacy.aether.tile_entities;
 
+import com.legacy.aether.Aether;
+
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class AetherTileEntities 
@@ -8,11 +10,11 @@ public class AetherTileEntities
 	@SuppressWarnings("deprecation")
 	public static void initialization()
 	{
-		GameRegistry.registerTileEntity(TileEntityEnchanter.class, "enchanter");
-		GameRegistry.registerTileEntity(TileEntityFreezer.class, "freezer");
-		GameRegistry.registerTileEntity(TileEntityIncubator.class, "incubator");
-		GameRegistry.registerTileEntity(TileEntityTreasureChest.class, "treasure_chest");
-		GameRegistry.registerTileEntity(TileEntityChestMimic.class, "chest_mimic");
+		GameRegistry.registerTileEntity(TileEntityEnchanter.class, Aether.modAddress() + "enchanter");
+		GameRegistry.registerTileEntity(TileEntityFreezer.class, Aether.modAddress() + "freezer");
+		GameRegistry.registerTileEntity(TileEntityIncubator.class, Aether.modAddress() + "incubator");
+		GameRegistry.registerTileEntity(TileEntityTreasureChest.class, Aether.modAddress() + "treasure_chest");
+		GameRegistry.registerTileEntity(TileEntityChestMimic.class, Aether.modAddress() + "chest_mimic");
 	}
 
 }
