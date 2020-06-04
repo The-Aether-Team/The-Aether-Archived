504ed7c610db957998b45caa7f7ed7c9978d7d45
bconlon <bconlon@gildedgames.com>
2020-06-04 15:23:53 -0700

    The Sun Altar no longer works outside of the Aether.

--- src/main/java/com/legacy/aether/blocks/BlockSunAltar.java
+++ src/main/java/com/legacy/aether/blocks/BlockSunAltar.java
@@ -1,5 +1,7 @@
 package com.legacy.aether.blocks;
 
+import com.legacy.aether.AetherConfig;
+import com.legacy.aether.world.AetherWorld;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -10,6 +12,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
@@ -31,19 +34,30 @@ public class BlockSunAltar extends Block
     {
 		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
 
-		if (server != null && ((server.isDedicatedServer() && server.getPlayerList().canSendCommands(player.getGameProfile()) && player.capabilities.isCreativeMode) || !server.isDedicatedServer()))
+		if (player.dimension == AetherConfig.dimension.aether_dimension_id)
 		{
-			Aether.proxy.openSunAltar();
-		}
-		else if (world.isRemote)
-		{
-			if (player instanceof EntityPlayerSP && ((EntityPlayerSP)player).getPermissionLevel() > 0 && player.capabilities.isCreativeMode)
+			if (server != null && ((server.isDedicatedServer() && server.getPlayerList().canSendCommands(player.getGameProfile()) && player.capabilities.isCreativeMode) || !server.isDedicatedServer()))
 			{
 				Aether.proxy.openSunAltar();
 			}
+			else if (world.isRemote)
+			{
+				if (player instanceof EntityPlayerSP && ((EntityPlayerSP)player).getPermissionLevel() > 0 && player.capabilities.isCreativeMode)
+				{
+					Aether.proxy.openSunAltar();
+				}
+			}
+
+		}
+		else
+		{
+			if (world.isRemote)
+			{
+				player.sendMessage(new TextComponentTranslation("gui.sun_altar.message"));
+			}
+
 		}
 
 		return true;
-    }
-
+	}
 }
