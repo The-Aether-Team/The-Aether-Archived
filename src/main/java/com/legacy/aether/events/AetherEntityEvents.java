3fcdf90664c3aac2e9d6e715188348841726fbd4
bconlon <bconlon@gildedgames.com>
2020-06-18 19:55:09 -0700

    The player will now be dismounted from any ridden entity.

--- src/main/java/com/legacy/aether/events/AetherEntityEvents.java
+++ src/main/java/com/legacy/aether/events/AetherEntityEvents.java
@@ -8,25 +8,39 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class AetherEntityEvents
 {
 	@SubscribeEvent
-	public void onEntityUpdate(net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent event)
+	public void onEntityUpdate(TickEvent.WorldTickEvent event)
 	{
-		if (event.getEntity() instanceof EntityLiving) entityUpdateEvents((EntityLiving) event.getEntity(), event);
+		if (event.phase == TickEvent.Phase.END)
+		{
+			if (!event.world.isRemote)
+			{
+				List<Entity> entityList = event.world.loadedEntityList;
+
+				for (int i = 0; i < entityList.size(); i++)
+				{
+					entityUpdateEvents(entityList.get(i), event);
+				}
+			}
+		}
 	}
 	
-	private void entityUpdateEvents(EntityLiving entity, Event event)
+	private void entityUpdateEvents(Entity entity, Event event)
 	{
-		if (entity instanceof EntitySaddleMount)
+		if (entity.isBeingRidden())
 		{
-			EntitySaddleMount mountable = (EntitySaddleMount) entity;
-			
-			if (entity.dimension == AetherConfig.dimension.aether_dimension_id && !entity.world.isRemote && mountable.getSaddled())
+			if (entity.dimension == AetherConfig.dimension.aether_dimension_id && !entity.world.isRemote)
 			{
 				MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
 				int previousDimension = entity.dimension;
@@ -40,7 +54,7 @@ public class AetherEntityEvents
 					}
 					
 					entity.timeUntilPortal = 300;
-					transferEntity(false, entity, server.getWorld(previousDimension), server.getWorld(transferDimension));	
+					transferEntity(false, entity, server.getWorld(previousDimension), server.getWorld(transferDimension));
 				}
 			}
 		}
