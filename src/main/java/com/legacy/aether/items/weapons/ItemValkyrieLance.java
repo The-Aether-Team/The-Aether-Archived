d1a9c9b6ee62a38444e5cffef2b137e591f5cefd
bconlon <bconlon@gildedgames.com>
2020-07-15 15:42:24 -0700

    Removed sweeping from Valkyrie Lances and lowered their attack speed by 0.2.

--- src/main/java/com/legacy/aether/items/weapons/ItemValkyrieLance.java
+++ src/main/java/com/legacy/aether/items/weapons/ItemValkyrieLance.java
@@ -1,34 +1,73 @@
 package com.legacy.aether.items.weapons;
 
+import java.util.HashMap;
 import java.util.List;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Enchantments;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
 import com.legacy.aether.items.ItemsAether;
 import com.legacy.aether.registry.creative_tabs.AetherCreativeTabs;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemValkyrieLance extends ItemSword
+public class ItemValkyrieLance extends Item
 {
+	private final float attackDamage;
+	private final Item.ToolMaterial material;
 
 	public ItemValkyrieLance()
 	{
-		super(ToolMaterial.DIAMOND);
-
+		this.material = ToolMaterial.DIAMOND;
+		this.maxStackSize = 1;
+		this.setMaxDamage(material.getMaxUses());
+		this.attackDamage = 3.0F + material.getAttackDamage();
 		this.setCreativeTab(AetherCreativeTabs.weapons);
 	}
 
 	@Override
-    public EnumRarity getRarity(ItemStack stack)
-    {
-    	return ItemsAether.aether_loot;
-    }
+	public EnumRarity getRarity(ItemStack stack)
+	{
+		return ItemsAether.aether_loot;
+	}
+
+	@Override
+	public float getDestroySpeed(ItemStack stack, IBlockState state)
+	{
+		Block block = state.getBlock();
+
+		if (block == Blocks.WEB)
+		{
+			return 15.0F;
+		}
+		else
+		{
+			Material material = state.getMaterial();
+			return material != Material.PLANTS && material != Material.VINE && material != Material.CORAL && material != Material.LEAVES && material != Material.GOURD ? 1.0F : 1.5F;
+		}
+	}
 
 	@Override
 	public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack)
@@ -96,8 +135,71 @@ public class ItemValkyrieLance extends ItemSword
 	}
 
 	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+	{
+		stack.damageItem(1, attacker);
+		return true;
+	}
+
+	@Override
+	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving)
+	{
+		if ((double)state.getBlockHardness(worldIn, pos) != 0.0D)
+		{
+			stack.damageItem(2, entityLiving);
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean canHarvestBlock(IBlockState blockIn)
+	{
+		return blockIn.getBlock() == Blocks.WEB;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean isFull3D()
+	{
+		return true;
+	}
+
+	@Override
+	public int getItemEnchantability()
+	{
+		return this.material.getEnchantability();
+	}
+
+	@Override
+	public boolean isBookEnchantable(ItemStack stack, ItemStack book)
+	{
+		return !EnchantmentHelper.getEnchantments(book).containsKey(Enchantments.SWEEPING);
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, net.minecraft.enchantment.Enchantment enchantment)
+	{
+
+		return !enchantment.equals(Enchantments.SWEEPING) && enchantment.type == EnumEnchantmentType.WEAPON;
+	}
+
+	@Override
 	public boolean getIsRepairable(ItemStack repairingItem, ItemStack material)
 	{
 		return false;
+	}
+
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
+	{
+		Multimap<String, AttributeModifier> multimap = HashMultimap.create();
+
+		if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
+		{
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", this.attackDamage, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.6000000953674316D, 0));
+		}
+
+		return multimap;
 	}
 }
