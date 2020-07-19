2c0eb888e785792fac9a3c08a3a6c8ff04761012
bconlon <bconlon@gildedgames.com>
2020-07-18 21:24:15 -0700

    Lightning Knives are now only stopped by full blocks.

--- src/main/java/com/legacy/aether/entities/projectile/EntityLightningKnife.java
+++ src/main/java/com/legacy/aether/entities/projectile/EntityLightningKnife.java
@@ -31,16 +31,27 @@ public class EntityLightningKnife extends EntityThrowable
 	{
 		if (!this.world.isRemote)
 		{
-			if (result.typeOfHit != Type.MISS)
+			if (result.typeOfHit == Type.ENTITY)
 			{
-				if (this.world instanceof WorldServer)
+				summonLightning(result);
+			}
+			else if (result.typeOfHit == Type.BLOCK)
+			{
+				if (this.world.getBlockState(result.getBlockPos()).isFullBlock())
 				{
-					((WorldServer)this.world).addWeatherEffect(new EntityLightningBolt(this.world, result.hitVec.x, result.hitVec.y, result.hitVec.z, false));
+					summonLightning(result);
 				}
 			}
-
-	        this.setDead();
 		}
 	}
 
+	private void summonLightning(RayTraceResult result)
+	{
+		if (this.world instanceof WorldServer)
+		{
+			((WorldServer)this.world).addWeatherEffect(new EntityLightningBolt(this.world, result.hitVec.x, result.hitVec.y, result.hitVec.z, false));
+		}
+
+		this.setDead();
+	}
 }
