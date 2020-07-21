58a0fce9e98e650d1ee303c517f7bd6093bb4db0
bconlon <bconlon@gildedgames.com>
2020-07-20 17:14:53 -0700

    Increased silver dungeon rarity again.

--- src/main/java/com/legacy/aether/world/gen/MapGenSilverDungeon.java
+++ src/main/java/com/legacy/aether/world/gen/MapGenSilverDungeon.java
@@ -24,9 +24,9 @@ public class MapGenSilverDungeon extends MapGenStructure {
 	protected boolean canSpawnStructureAtCoords(int chunkX, int chunkZ) {
 		RandomTracker randomTracker = new RandomTracker();
 
-		if (randomTracker.testRandom(this.rand, 100) != 0)
+		if (randomTracker.testRandom(this.rand, 120) != 0)
 		{
-			if (randomTracker.testRandom(this.rand, 140) != 0)
+			if (randomTracker.testRandom(this.rand, 160) != 0)
 			{
 				return false;
 			}
