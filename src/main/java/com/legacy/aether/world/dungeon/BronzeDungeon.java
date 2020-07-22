2756d5ca795aa0f64cc154c34ce30ec97df3da01
bconlon <bconlon@gildedgames.com>
2020-07-21 23:59:03 -0700

    Spawnrates.

--- src/main/java/com/legacy/aether/world/dungeon/BronzeDungeon.java
+++ src/main/java/com/legacy/aether/world/dungeon/BronzeDungeon.java
@@ -48,9 +48,9 @@ public class BronzeDungeon extends AetherDungeon {
 
 		RandomTracker randomTracker = new RandomTracker();
 
-		if (randomTracker.testRandom(random,40) != 0)
+		if (randomTracker.testRandom(random,25) != 0)
 		{
-			if (randomTracker.testRandom(random,60) != 0)
+			if (randomTracker.testRandom(random,55) != 0)
 			{
 				return false;
 			}
