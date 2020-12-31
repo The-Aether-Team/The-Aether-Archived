2d8b84b9f697fd7c9cb60a94a9d0673c3edf4330
bconlon <bconlon@gildedgames.com>
2020-12-30 20:19:46 -0800

    Reduced spawning requirements for the Valkyrie Queen.

--- src/main/java/com/gildedgames/the_aether/entities/bosses/valkyrie_queen/EntityValkyrieQueen.java
+++ src/main/java/com/gildedgames/the_aether/entities/bosses/valkyrie_queen/EntityValkyrieQueen.java
@@ -555,7 +555,7 @@ public class EntityValkyrieQueen extends EntityBossMob implements IAetherBoss {
         int i = MathHelper.floor_double(this.posX);
         int j = MathHelper.floor_double(this.boundingBox.minY);
         int k = MathHelper.floor_double(this.posZ);
-        return this.worldObj.getBlockLightValue(i, j, k) > 8 && this.worldObj.checkBlockCollision(this.boundingBox) && this.worldObj.getCollidingBoundingBoxes(this, this.boundingBox).size() == 0 && !this.worldObj.isAnyLiquid(this.boundingBox);
+        return this.worldObj.checkBlockCollision(this.boundingBox) && this.worldObj.getCollidingBoundingBoxes(this, this.boundingBox).size() == 0;
     }
 
     public int getMedals(EntityPlayer entityplayer) {
