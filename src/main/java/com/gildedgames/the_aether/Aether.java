63e9a73e80a990c5ae654c925d29b0062f4c22b5
bconlon <bconlon@gildedgames.com>
2021-04-16 17:38:31 -0700

    Version bump to 1.1.2.2.

--- src/main/java/com/gildedgames/the_aether/Aether.java
+++ src/main/java/com/gildedgames/the_aether/Aether.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Aether.MOD_ID, version = "v1.1.2.1")
+@Mod(modid = Aether.MOD_ID, version = "v1.1.2.2")
 public class Aether {
 
 	public static final String MOD_ID = "aether_legacy";
